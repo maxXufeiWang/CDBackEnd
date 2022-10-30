d4017b219b9f9f74b354be63b115f5713d1e4eed
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-30 19:48:37 +0000

    Adding tarfile member sanitization to extractall()

--- crypto-detector-master/cryptodetector/filelister.py
+++ crypto-detector-master/cryptodetector/filelister.py
@@ -536,7 +536,26 @@ class FileLister():
         Output.print_information("Extracting tar archive " + display_path + " ...")
         try:
             with tarfile.open(tar_file_path) as tar_file:
-                tar_file.extractall(output_directory)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar_file, output_directory)
         except Exception as expn:
             raise ExtractError("Failed to extract tar archive " + display_path + "\n" + str(expn))
 
